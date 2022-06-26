62ddeacd6c9ac1ba82824f599de1fa3492ec416c
dhf <1049334207@qq.com>
2022-06-26 13:48:16 +0800

    first commit by IDEA

---
+++ src/main/java/com/local/dhf/git/Upload_one.java
@@ -0,0 +1,16 @@
+package com.local.dhf.git;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author 党
+ * @version 1.0
+ * 2022/6/26   13:47
+ */
+public class Upload_one{
+    public static void main(String[] args) {
+
+    }
+}
+
